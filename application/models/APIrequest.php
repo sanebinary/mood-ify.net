7730f6768ea0be46ca8e53eb5c022c5c335e86fa
unknown <anh.nguyenduc259wk@gmail.com>
2020-02-03 21:42:33 +0700

    make php curl not output response

--- application/models/APIrequest.php
+++ application/models/APIrequest.php
@@ -17,6 +17,7 @@ class APIrequest extends CI_Model
         $curl = curl_init();
         $recom_url = $endpoint. '?'. $seeds;
         curl_setopt($curl, CURLOPT_URL,           $recom_url );
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1 );
         curl_setopt($curl, CURLOPT_HTTPHEADER,     array('Authorization: Bearer '.$token));
         $result=curl_exec($curl);
         return $result;
