9e907ce122e9dd30c820a38f649843ea43422b9c
unknown <anh.nguyenduc259wk@gmail.com>
2020-04-29 02:21:08 +0700

    Add user database model

---
+++ application/models/User.php
@@ -0,0 +1,50 @@
+<?php 
+
+class User extends CI_Model {
+
+    protected $User_table_name = "users";
+
+    /**
+     * Insert User Data in Database
+     * @param: {array} userData
+     */
+    public function insert_user($userData) {
+        return $this->db->insert($this->User_table_name, $userData);
+    }
+
+    /**
+     * Check User Login in Database
+     * @param: {array} userData
+     */
+    public function check_login($userData) {
+
+        /**
+         * First Check Email is Exists in Database
+         */
+        $query = $this->db->get_where($this->User_table_name, array('email' => $userData['email']));
+        if ($this->db->affected_rows() > 0) {
+
+            $password = $query->row('password');
+
+            /**
+             * Check Password Hash 
+             */
+            if (password_verify($userData['password'], $password) === TRUE) {
+
+                /**
+                 * Password and Email Address Valid
+                 */
+                return [
+                    'status' => TRUE,
+                    'data' => $query->row(),
+                ];
+
+            } else {
+                return ['status' => FALSE,'data' => FALSE];
+            }
+
+        } else {
+            return ['status' => FALSE,'data' => FALSE];
+        }
+    }
+}
