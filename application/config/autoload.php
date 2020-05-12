c633f0f8a21b53bb793679f87c35e3de516a51c3
unknown <anh.nguyenduc259wk@gmail.com>
2020-05-12 16:01:11 +0700

    [TEMP CHANGE] Disable database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,4 +133,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array();
-$autoload['libraries'] = array('form_validation', 'database', 'session');
+//$autoload['libraries'] = array('form_validation', 'database', 'session');
